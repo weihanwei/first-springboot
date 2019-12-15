70d81291677b9cfd483b2605d02fd4f0121f2461
weihanwei <407427270@qq.com>
2019-12-14 22:31:34 -0500

    20191214

--- src/main/java/com/example/demo/HellowWork.java
+++ src/main/java/com/example/demo/HellowWork.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import java.io.File;
+import java.util.Properties;
+
+import org.python.util.PythonInterpreter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +21,8 @@ public class HellowWork {
 	@Value("${age}")
 	private Integer age;
 	
+	//@Value("${pythonFileUrl}")
+	private String pythonFileUrl;
 	
 	@RequestMapping("/index/{name}")
     @ResponseBody
@@ -40,5 +47,48 @@ public class HellowWork {
 	@RequestMapping("/getName")
 	public String getName(String name) {
 		return name;
+	}
+	/**
+	 * http://localhost:9394/springbootDemo/test/testPy
+	 * @return
+	 */
+	@RequestMapping("/testPy")
+	public String testPy() {
+		 Runtime rt = Runtime.getRuntime();
+
+		
+		
+	        try {
+	        	/*
+	        	File path = new File(ResourceUtils.getURL("classpath:").getPath());
+	    		if(!path.exists()) path = new File("");
+	    		System.out.println("path:"+path.getAbsolutePath());
+	    		*/
+		        Process proc = rt.exec(pythonFileUrl+"/test.py");
+	        }catch (Exception e){
+	            e.printStackTrace();
+	        }
+	
+		return name;
+	}
+	
+	
+	public static void main(String[] args) {
+		Properties props = new Properties();
+        props.put("python.home", "path to the Lib folder");
+        props.put("python.console.encoding", "UTF-8");
+
+        props.put("python.security.respectJavaAccessibility", "false");
+
+        props.put("python.import.site", "false");
+
+        Properties preprops = System.getProperties();
+
+		
+		PythonInterpreter itp = new PythonInterpreter();
+		itp.exec("days=('Mod','Tue','Wed','Thu','Fri','Sat','Sun'); ");
+		itp.exec("print days[1];");
+		itp.execfile("/tmp/test.py");
+		itp.exec("print 'created by tengxing on 2017.3'");
 	}
 }
